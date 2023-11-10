31b696ced717ca4a59f100df847360612bc31b68
Emma Walker <walkere@tcd.ie>
2023-11-10 09:47:41 +0000

    Initial Commit

---
+++ PlaybackInSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PlaybackInSwiftUI
+//
+//  Created by Emma Walker - TVandMobile Platforms - Core Engineering on 10/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlaybackInSwiftUI/PlaybackInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaybackInSwiftUIApp.swift
+//  PlaybackInSwiftUI
+//
+//  Created by Emma Walker - TVandMobile Platforms - Core Engineering on 10/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlaybackInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
