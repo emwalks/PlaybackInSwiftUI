2f53ca782fdfb3adaa60658bdc9fd9aeff24a59f
Emma Walker <walkere@tcd.ie>
2023-11-24 13:00:17 +0000

    Adds more precise seek and discussion of live stream playlists

--- PlaybackInSwiftUI/AVPlayerBridgedViewController/AVPlayerVCFullscreenLive.swift
+++ PlaybackInSwiftUI/AVPlayerBridgedViewController/AVPlayerVCFullscreenLive.swift
@@ -12,16 +12,17 @@ import AVKit
  This is an example of:
  - A fullscreen player playing back live content
  - When loaded, the content is always playing at the live edge
- - This is an example of interacting with the player items seekable range
+ - This is an example of interacting with the player items' seekable range
  */
-
-// https://www.hackingwithswift.com/quick-start/swiftui/how-to-present-a-full-screen-modal-view-using-fullscreencover
-// https://stackoverflow.com/questions/13145048/hls-avplayer-on-ios-return-to-live
 
 struct AVPlayerVCFullscreenLive: View {
     
-    // Live Akamai m3u8 HLS stream
-    @State var player = AVPlayer(url: URL(string: "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8")!)
+    @State var player = AVPlayer(url: URL(string:
+                                          // There is an issue with the live edge/ seekable on this stream. "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8"
+                                          // BBC One live (geo ip restricted) stream"http://vs-hls-push-uk-live.akamaized.net/x=4/i=urn:bbc:pips:service:bbc_one_hd/mobile_wifi_main_sd_abr_v2.m3u8"
+                                         // BBC News 24
+                                          "https://vs-hls-push-uk-live.akamaized.net/x=4/i=urn:bbc:pips:service:bbc_news_channel_hd/mobile_wifi_main_sd_abr_v2.m3u8"
+                                         )!)
     
     var body: some View {
         AVPlayerView(player: player)
@@ -32,8 +33,9 @@ struct AVPlayerVCFullscreenLive: View {
                      return
                  }
                 let livePosition = CMTimeRangeGetEnd(seekableRange)
-                // TODO: check tolerance seek option as this does not always seek to live edge
-                player.seek(to: livePosition)
+                // Add more precise seek to ensure you seek to live edge.
+                // Though there is a time penalty to this - https://developer.apple.com/documentation/avfoundation/media_playback/controlling_the_transport_behavior_of_a_player
+                player.seek(to: livePosition, toleranceBefore: .zero, toleranceAfter: .zero)
                 player.play()
             }
             .onDisappear{
